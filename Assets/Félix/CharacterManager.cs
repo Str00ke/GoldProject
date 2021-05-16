dba71d9e4d16509e6a0f245063a26e657a0444b0
Zeldruck <felix.jouannneau@gmail.com>
2021-05-16 19:47:07 +0200

    feat: item stats fill character stats

--- Assets/Félix/CharacterManager.cs
+++ Assets/Félix/CharacterManager.cs
@@ -4,37 +4,24 @@ using UnityEngine;
 
 public class CharacterManager : MonoBehaviour
 {
-    private Character char1;
-    private Character char2;
-    private Character char3;
+    private Character[] characters;
 
     public static CharacterManager characterManager;
 
     private void Awake()
     {
         characterManager = this;
+        characters = new Character[3];
     }
 
-    public Character AskForCharacter(int character)
+    public Character AskForCharacter(int indexChar)
     {
-        if (character == 1)
-            return char1;
-        else if (character == 2)
-            return char2;
-        if (character == 3)
-            return char3;
-
-        return null;
+        return characters[indexChar];
     }
 
-    public void SetCharacter(int index, Character character)
+    public void SetCharacter(int indexChar, Character character)
     {
-        if (index == 1)
-            char1 = character;
-        else if (index == 2)
-            char2 = character;
-        if (index == 3)
-            char3 = character;
+        characters[indexChar] = character;
     }
 
     public void SummonCharacter()
@@ -43,13 +30,28 @@ public class CharacterManager : MonoBehaviour
         nGameObject.transform.parent = gameObject.transform;
         Character nChar = nGameObject.AddComponent<Character>();
 
-        if (char1 == null)
-            char1 = nChar;
-        else if (char2 == null)
-            char2 = nChar;
-        else if (char3 == null)
-            char3 = nChar;
-        else
+        bool isSet = false;
+
+        for (int i = 0; i < characters.Length; i++)
+        {
+            if (characters[i] == null)
+            {
+                characters[i] = nChar;
+                isSet = true;
+                break;
+            }
+        }
+
+        if (!isSet)
+        {
             Destroy(nGameObject);
+        }
+    }
+
+    public void SwapCharacterIndex(int indexChar1, int indexChar2)
+    {
+        Character charRef = characters[indexChar1];
+        characters[indexChar1] = characters[indexChar2];
+        characters[indexChar2] = charRef;
     }
 }
