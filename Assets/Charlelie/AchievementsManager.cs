e4512594bf8a7262510f6c6fa8a67a75c978633e
Str00ke <charlelie.garrigues@edu.devinci.fr>
2021-05-28 17:13:10 +0200

    fix build error

--- Assets/Charlelie/AchievementsManager.cs
+++ Assets/Charlelie/AchievementsManager.cs
@@ -2,11 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SocialPlatforms;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
 
 public static class AchievementsManager
 {
     public static void Logged()
-    {
-        Social.ReportProgress("CgkIv6KQjKwJEAIQCQ", 100.00, (bool success) => { });
+    {   
+        Social.ReportProgress(GPGSIds.achievement_logged, 100.00f, (bool success) =>
+        {
+            if (success)
+                Debug.Log("Achievement unlocked");
+            else
+                Debug.Log("Achievement problem");
+        });
     }
 }
