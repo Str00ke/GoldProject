3e0fb0b1ee3cb7016f656c4256d879cc5e293208
AlexCopin <alexcopin@live.fr>
2021-06-15 16:38:40 +0200

    fix: critic

--- Assets/Alex/CharactersScripts/Ally.cs
+++ Assets/Alex/CharactersScripts/Ally.cs
@@ -189,7 +189,7 @@ public class Ally : Characters
         else
         {
             //-CRITIC DAMAGE-
-            if (Random.Range(0.0f, 1.0f) < this.critChance/100)
+            if (Random.Range(0, 100) < this.critChance)
             {
                 FindObjectOfType<CameraScript>().CamShake(0.4f, 0.3f);
                 dmg += dmg * this.critDamage/100;
